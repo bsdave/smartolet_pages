efb485a7557f1555333b087d646205e51103ac96
Sergey Balabanov <obs.balabanov@gmail.com>
2021-10-25 16:48:48 +0600

    Made loan calculator

--- src/uncompiled_js/main.js
+++ src/uncompiled_js/main.js
@@ -1,14 +1,16 @@
 $(function () {
   $('html').addClass('js');
 
+  initializeRanges();
+
   new Swiper('.swiper.swiper_paginate-dots', {
     loop: true,
-  
+
     pagination: {
       el: '.swiper-pagination',
     },
   });
-  
+
   new Swiper('.swiper.swiper_paginate-fraction', {
     pagination: {
       el: '.swiper-pagination',
@@ -17,13 +19,36 @@ $(function () {
         return ('0' + number).slice(-2);
       },
       formatFractionTotal: function (number) {
-          return ('0' + number).slice(-2);
+        return ('0' + number).slice(-2);
       },
     },
-  
+
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   });
 });
+
+
+const initializeRanges = () => {
+  $('.range').each(function () {
+    recalculateRangeValue(this);
+  });
+
+  $('.range').on('input', function () {
+    recalculateRangeValue(this);
+  });
+};
+
+const recalculateRangeValue = (range) => {
+  const min = range.min;
+  const max = range.max - min;
+  const current = range.value - min;
+  const percentage = current * 100 / max;
+  const output = $(range).parents('.calculator__label-box').find('.input.bold');
+
+  $(range).css('background', `linear-gradient(90deg, #59AEFF, #59AEFF, #59AEFF ${percentage}%, transparent ${percentage}%, transparent 100%)`);
+  output.val(`${range.value} ${output.data().symbol}`);
+}
+
